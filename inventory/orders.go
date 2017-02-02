2991c5a7b387ec318bcfa5118b8428c2548a14c0
Sean Devlin <sdevlin@sevone.com>
2017-02-02 00:44:00 -0500

    Refactored workflow to behave better when reporting is added

--- inventory/orders.go
+++ inventory/orders.go
@@ -3,7 +3,6 @@ package inventory
 import (
 	"errors"
 	"fmt"
-	//"strings"
 )
 
 type Item struct {
@@ -16,22 +15,32 @@ func NewItem(BuyPrice, SellPrice int) Item {
 	return Item{Qty: 0, BuyPrice: BuyPrice, SellPrice: SellPrice}
 }
 
+/**
+A utitlity function for pretty-printing the currency
+*/
 func RenderCurrency(Currency int) string {
 	return fmt.Sprintf("%d.%02d", Currency/100, Currency%100)
 }
 
-type Command int
-
 type StateEntry interface {
-	NextState(accum map[string]Item) (map[string]Item, int, error)
+	//NextState(accum map[string]Item) (map[string]Item, int, error)
+	NextState(accum State) (State, error)
 	RenderEntry() string
 }
 
-//The Identity order is explicitly included for unit testing
+type State struct {
+	Items   map[string]Item
+	Revenue int
+	Cost    int
+}
+
+/**
+* IDENTITY FAMILY - For Testing
+**/
 type IdentityOrder struct{}
 
-func (this *IdentityOrder) NextState(accum map[string]Item) (map[string]Item, int, error) {
-	return accum, 0, nil
+func (this *IdentityOrder) NextState(accum State) (State, error) {
+	return accum, nil
 }
 
 func (this *IdentityOrder) RenderEntry() string {
@@ -42,22 +51,26 @@ func NewIdentityOrder() StateEntry {
 	return new(IdentityOrder)
 }
 
+/**
+* BUY FAMILY
+**/
 type BuyOrder struct {
 	ItemName string
 	Quantity int
 }
 
-func (this *BuyOrder) NextState(accum map[string]Item) (map[string]Item, int, error) {
+func (this *BuyOrder) NextState(accum State) (State, error) {
 	if this.Quantity < 0 {
-		return accum, 0, errors.New("Negative Buy Attempted: " + this.RenderEntry())
+		return accum, errors.New("Negative Buy Attempted: " + this.RenderEntry())
 	}
-	if _, ok := accum[this.ItemName]; ok == false {
-		return accum, 0, errors.New("Buying Non-Existent Item Attempted: " + this.RenderEntry())
+	if _, ok := accum.Items[this.ItemName]; ok == false {
+		return accum, errors.New("Buying Non-Existent Item Attempted: " + this.RenderEntry())
 	}
-	i := accum[this.ItemName]
+	i := accum.Items[this.ItemName]
 	i.Qty += this.Quantity
-	accum[this.ItemName] = i
-	return accum, -this.Quantity * i.BuyPrice, nil
+	accum.Items[this.ItemName] = i
+	accum.Cost += this.Quantity * i.BuyPrice
+	return accum, nil
 }
 
 func (this *BuyOrder) RenderEntry() string {
@@ -68,99 +81,106 @@ func NewBuyOrder(ItemName string, Quantity int) StateEntry {
 	return &BuyOrder{ItemName: ItemName, Quantity: Quantity}
 }
 
+/**
+* SELL FAMILY
+**/
 type SellOrder struct {
 	ItemName string
 	Quantity int
 }
 
-func (this *SellOrder) NextState(accum map[string]Item) (map[string]Item, int, error) {
+func (this *SellOrder) NextState(accum State) (State, error) {
 	if this.Quantity < 0 {
-		return accum, 0, errors.New("Negative Sell Attempted: " + this.RenderEntry())
+		return accum, errors.New("Negative Sell Attempted: " + this.RenderEntry())
 	}
-	if _, ok := accum[this.ItemName]; ok == false {
-		return accum, 0, errors.New("Selling Non-Existent Item Attempted: " + this.RenderEntry())
+	if _, ok := accum.Items[this.ItemName]; ok == false {
+		return accum, errors.New("Selling Non-Existent Item Attempted: " + this.RenderEntry())
 	}
-	i := accum[this.ItemName]
+	i := accum.Items[this.ItemName]
 	if i.Qty < this.Quantity {
-		return accum, 0, errors.New("Selling too Many Units Attempted: " + this.RenderEntry())
+		return accum, errors.New("Selling too Many Units Attempted: " + this.RenderEntry())
 	}
 	i.Qty -= this.Quantity
-	accum[this.ItemName] = i
-	return accum, this.Quantity * i.SellPrice, nil
+	accum.Items[this.ItemName] = i
+	accum.Revenue += this.Quantity * i.SellPrice
+	return accum, nil
 }
 
 func (this *SellOrder) RenderEntry() string {
 	return fmt.Sprintf("updateSell %s %d", this.ItemName, this.Quantity)
 }
 
-func NewSellOrder(ItemName string, Quantity int) StateEntry {
-	return &SellOrder{ItemName: ItemName, Quantity: Quantity}
+func NewSellOrder(itemName string, quantity int) StateEntry {
+	return &SellOrder{ItemName: itemName, Quantity: quantity}
 }
 
+/**
+* CREATE FAMILY
+**/
 type Create struct {
 	ItemName  string
 	BuyPrice  int
 	SellPrice int
 }
 
-func (this *Create) NextState(accum map[string]Item) (map[string]Item, int, error) {
-	if _, ok := accum[this.ItemName]; ok {
-		return accum, 0, errors.New("Cannot create previously existing item " + this.RenderEntry())
+/**
+* The create entry will create a new type of item for sale when it's encountered in the journal.  This method will return an error if it encouners a pre-existing item in the accum table.  It always returns a change or zero.
+ */
+func (this *Create) NextState(accum State) (State, error) {
+	if _, ok := accum.Items[this.ItemName]; ok {
+		return accum, errors.New("Cannot create previously existing item " + this.RenderEntry())
 	}
-	accum[this.ItemName] = NewItem(this.BuyPrice, this.SellPrice)
-	return accum, 0, nil
+	accum.Items[this.ItemName] = NewItem(this.BuyPrice, this.SellPrice)
+	return accum, nil
 }
 
 func (this *Create) RenderEntry() string {
 	return fmt.Sprintf("create %s %s %s", this.ItemName, RenderCurrency(this.BuyPrice), RenderCurrency(this.SellPrice))
 }
 
-func NewCreate(ItemName string, BuyPrice, SellPrice int) StateEntry {
-	return &Create{ItemName: ItemName, BuyPrice: BuyPrice, SellPrice: SellPrice}
+func NewCreate(itemName string, buyPrice, sellPrice int) StateEntry {
+	return &Create{ItemName: itemName, BuyPrice: buyPrice, SellPrice: sellPrice}
 }
 
+/**
+* DELETE FAMILY
+**/
 type Delete struct {
 	ItemName string
 }
 
-func (this *Delete) NextState(accum map[string]Item) (map[string]Item, int, error) {
-	if _, ok := accum[this.ItemName]; !ok {
-		return accum, 0, errors.New("Cannot delete non-existent item " + this.RenderEntry())
+/**
+* The delete entry will delete an existing item for sale when it's encountered in the journal.  This method will return an error if it attempts to delete an item that doesn't exist.
+ */
+func (this *Delete) NextState(accum State) (State, error) {
+	if _, ok := accum.Items[this.ItemName]; !ok {
+		return accum, errors.New("Cannot delete non-existent item " + this.RenderEntry())
 	}
-	delete(accum, this.ItemName)
-	return accum, 0, nil
+	delete(accum.Items, this.ItemName)
+	return accum, nil
 }
 
 func (this *Delete) RenderEntry() string {
 	return fmt.Sprintf("delete %s", this.ItemName)
 }
 
-func NewDelete(ItemName string) StateEntry {
-	return &Delete{ItemName: ItemName}
+func NewDelete(itemName string) StateEntry {
+	return &Delete{ItemName: itemName}
 }
 
-func ProcessJournal(accum map[string]Item, entries []StateEntry) {
-	revenue, cost := 0, 0
-	for _, entry := range entries {
-		change := 0
-		var err error = nil
-		accum, change, err = entry.NextState(accum)
-		if err != nil {
-			continue
-		}
-		if change > 0 {
-			revenue += change
-		} else if change < 0 {
-			cost += change
-		}
-	}
-}
-
-//func ProcessEntry(accum map[string]Item, elem Entry) map[string]Item, error{
-//switch e := elem.(type) {
-//case Order:
-//case Manage:
-//default:
-//return accum, errors.New("Unknown type provided");
+//func ProcessJournal(accum map[string]Item, entries []StateEntry) {
+//revenue, cost := 0, 0
+//for _, entry := range entries {
+//change := 0
+//var err error = nil
+//accum, change, err = entry.NextState(accum)
+//if err != nil {
+//continue
+//}
+//if change > 0 {
+//revenue += change
+//} else if change < 0 {
+//cost += change
+//}
 //}
 //}
--- inventory/orders_test.go
+++ inventory/orders_test.go
@@ -29,16 +29,14 @@ func _TestOrderRenderEntry(t *testing.T, order StateEntry, expected string) {
 func TestIdentityNextState(t *testing.T) {
 	_TestOrderNextState(t,
 		NewIdentityOrder(),
-		map[string]Item{},
-		map[string]Item{},
-		0,
+		State{Items: map[string]Item{}},
+		State{Items: map[string]Item{}},
 		nil,
 	)
 	_TestOrderNextState(t,
 		NewIdentityOrder(),
-		map[string]Item{"A": Item{}},
-		map[string]Item{"A": Item{}},
-		0,
+		State{Items: map[string]Item{"A": Item{}}},
+		State{Items: map[string]Item{"A": Item{}}},
 		nil,
 	)
 }
@@ -47,17 +45,15 @@ func TestCreateNextState(t *testing.T) {
 	//This tests the case where a new item is being inserted to an empty state
 	_TestOrderNextState(t,
 		NewCreate("A", 1, 1),
-		map[string]Item{},
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		0,
+		State{Items: map[string]Item{}},
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
 		nil,
 	)
 	//This tests the case where an item is being inserted to an empty state.  Notice that the buy & sell price are NOT updated
 	_TestOrderNextState(t,
 		NewCreate("A", 2, 2),
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		0,
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
 		errors.New("Cannot create previously existing item create A 0.02 0.02"),
 	)
 }
@@ -66,17 +62,15 @@ func TestDeleteNextState(t *testing.T) {
 	//This tests the case where a new item is being inserted to an empty state
 	_TestOrderNextState(t,
 		NewDelete("A"),
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		map[string]Item{},
-		0,
+		State{Items: map[string]Item{"A": Item{}}},
+		State{Items: map[string]Item{}},
 		nil,
 	)
 	//This tests the case where an item is being inserted to an empty state.  Notice that the buy & sell price are NOT updated
 	_TestOrderNextState(t,
 		NewDelete("A"),
-		map[string]Item{},
-		map[string]Item{},
-		0,
+		State{Items: map[string]Item{}},
+		State{Items: map[string]Item{}},
 		errors.New("Cannot delete non-existent item delete A"),
 	)
 }
@@ -85,32 +79,29 @@ func TestBuyNextState(t *testing.T) {
 	//This tests the case where a new item is being inserted to an empty state
 	_TestOrderNextState(t,
 		NewBuyOrder("A", 1),
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1, Qty: 1}},
-		-1,
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1, Qty: 1}}, Cost: 1},
 		nil,
 	)
+	//This case show that buying a negative qty generates an error
 	_TestOrderNextState(t,
 		NewBuyOrder("A", -1),
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		0,
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
 		errors.New("Negative Buy Attempted: updateBuy A -1"),
 	)
 	//This case shows that buying a non-existent items generates an error
 	_TestOrderNextState(t,
 		NewBuyOrder("B", 1),
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		0,
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
 		errors.New("Buying Non-Existent Item Attempted: updateBuy B 1"),
 	)
 	//This case shows that the negative buy error trumps the non-existent buy error
 	_TestOrderNextState(t,
 		NewBuyOrder("B", -1),
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		0,
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
 		errors.New("Negative Buy Attempted: updateBuy B -1"),
 	)
 }
@@ -119,41 +110,35 @@ func TestSellNextState(t *testing.T) {
 	//This tests the case where a new item is being inserted to an empty state
 	_TestOrderNextState(t,
 		NewSellOrder("A", 1),
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1, Qty: 1}},
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		1,
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1, Qty: 1}}},
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}, Revenue: 1},
 		nil,
 	)
-	//This case shows that selling a negative number of items generates an error
+	//This case show that buying a negative qty generates an error
 	_TestOrderNextState(t,
 		NewSellOrder("A", -1),
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		0,
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
 		errors.New("Negative Sell Attempted: updateSell A -1"),
 	)
-	//This case shows that selling non-existent items raises an error
+	//This case shows that buying a non-existent items generates an error
 	_TestOrderNextState(t,
 		NewSellOrder("B", 1),
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		0,
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
 		errors.New("Selling Non-Existent Item Attempted: updateSell B 1"),
 	)
 	//This case shows that the negative buy error trumps the non-existent buy error
 	_TestOrderNextState(t,
 		NewSellOrder("B", -1),
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		0,
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
 		errors.New("Negative Sell Attempted: updateSell B -1"),
 	)
-	//This case shows that selling too many of an item generates an error
 	_TestOrderNextState(t,
 		NewSellOrder("A", 1),
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}},
-		0,
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
+		State{Items: map[string]Item{"A": Item{BuyPrice: 1, SellPrice: 1}}},
 		errors.New("Selling too Many Units Attempted: updateSell A 1"),
 	)
 }
@@ -161,13 +146,16 @@ func TestSellNextState(t *testing.T) {
 /*
 This is a worker function that does a lot of the heavy lifting for the testing.  It takes in the input state, and the next step, and has expect output state, delta, and error
 */
-func _TestOrderNextState(t *testing.T, order StateEntry, input, expectedAccum map[string]Item, expectedDelta int, expectedError error) {
-	actualAccum, actualDelta, actualError := order.NextState(input)
-	if actualDelta != expectedDelta {
-		t.Errorf("Expected Delta:%d,\tActual Delta%d", expectedDelta, actualDelta)
+func _TestOrderNextState(t *testing.T, order StateEntry, input, expectedAccum State, expectedError error) {
+	actualAccum, actualError := order.NextState(input)
+	if actualAccum.Cost != expectedAccum.Cost {
+		t.Errorf("Expected Cost:%d,\tActual Cost%d", expectedAccum.Cost, actualAccum.Cost)
 	}
-	for actualKey, actualValue := range actualAccum {
-		expectedValue, ok := expectedAccum[actualKey]
+	if actualAccum.Revenue != expectedAccum.Revenue {
+		t.Errorf("Expected Revenue:%d,\tActual Revenue%d", expectedAccum.Revenue, actualAccum.Revenue)
+	}
+	for actualKey, actualValue := range actualAccum.Items {
+		expectedValue, ok := expectedAccum.Items[actualKey]
 		if ok == false {
 			t.Errorf("key %v not found in expected :(", actualKey)
 		}
