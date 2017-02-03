0bf1a6a2fcb79f0dc80e50c102eaa1c834146a12
Sean Devlin <sdevlin@sevone.com>
2017-02-02 23:38:48 -0500

    Added sample report test

--- inventory/render_test.go
+++ inventory/render_test.go
@@ -19,3 +19,20 @@ func _TestRenderCurrency(t *testing.T, currency int, expected string) {
 		t.Errorf("Currency:\t%d,\tActual String:\t%s,\tExpected String:\t%s", currency, actual, expected)
 	}
 }
+
+func TestRenderState(t *testing.T) {
+	s := State{Items: map[string]Item{"A": NewItem(1, 1)}}
+	expected := `Name                    Quantity  Bought At    Sold At    Bought Value
+-----                      -----      -----      -----           -----
+A                              0       0.01       0.01            0.00
+-----                      -----      -----      -----           -----
+                              Inventory Value                     0.00
+                              Revenue Since Last Report           0.00
+                              Cost Since Last Report              0.00
+                              Profit Since Last Report            0.00
+`
+	actual := RenderState(s)
+	if expected != actual {
+		t.Errorf("Expected Report:\n%v\n,Actual Report:\n%v\n", expected, actual)
+	}
+}
